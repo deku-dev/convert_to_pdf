2bddbf6ad8e979dd2b9293a9dc15a1b5fe153a70
Deku <v.hurych@internetdevels.com>
2022-06-06 18:11:45 +0300

    Render page as html

--- src/Controller/StudentPdfController.php
+++ src/Controller/StudentPdfController.php
@@ -3,10 +3,13 @@
 namespace Drupal\student_pdf\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\student_pdf\Entity\Student;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Session\AccountProxyInterface;
-use Drupal\node\NodeInterface;
 use mikehaertl\wkhtmlto\Pdf;
+use Symfony\Component\HttpFoundation\Response;
+use Drupal\file\Entity\File;
+use Drupal\Core\ProxyClass\Render\BareHtmlPageRenderer;
 
 /**
  * Class StudentPdfController.
@@ -14,13 +17,30 @@ use mikehaertl\wkhtmlto\Pdf;
 class StudentPdfController extends ControllerBase {
 
   /**
+   * Current user object.
+   *
+   * @var \Drupal\Core\Session\AccountProxy
+   */
+  protected $currentUser;
+
+  /**
+   * Render bare page.
+   *
+   * @var \Drupal\Core\ProxyClass\Render\BareHtmlPageRenderer
+   */
+  protected $renderPage;
+
+  /**
    * Construct.
    *
    * @param \Drupal\Core\Session\AccountProxyInterface $currentUser
    *   Current user data.
+   * @param \Drupal\Core\ProxyClass\Render\BareHtmlPageRenderer $renderPage
+   *   Render bare page service.
    */
-  public function __construct(AccountProxyInterface $currentUser) {
+  public function __construct(AccountProxyInterface $currentUser, BareHtmlPageRenderer $renderPage) {
     $this->currentUser = $currentUser;
+    $this->renderPage = $renderPage;
   }
 
   /**
@@ -34,7 +54,8 @@ class StudentPdfController extends ControllerBase {
    */
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('current_user')
+      $container->get('current_user'),
+      $container->get('bare_html_page_renderer')
     );
   }
 
@@ -42,17 +63,48 @@ class StudentPdfController extends ControllerBase {
    * Build pdf version.
    * @param $student
    *   Student entity.
-   * @return array
+   *
+   * @return \Symfony\Component\HttpFoundation\Response
    *   Render array.
    */
-  public function build($student) {
-    $render = [
-      '#theme' => 'student_pdf',
-      '#firstname' => $student->getFirstName(),
-      '#lastname' => $student->getLastName(),
-      '#content' => $student->getBio(),
-    ];
-    return $render;
+  public function build(Student $student) {
+    $renderedPage = $this->renderPage
+      ->renderBarePage([], $this->t('Student pdf'), 'student_pdf', [
+        '#firstname' => $student->getFirstName(),
+        '#lastname' => $student->getLastName(),
+        '#bio' => ['#markup' => $student->getBio()],
+        '#photo' => $this->getImageUri($student, 'photo'),
+        '#show_messages' => FALSE,
+      ])
+      ->getContent();
+    $pdf = new Pdf;
+    $pdf->addPage($renderedPage);
+    if (!$pdf->saveAs('./page.pdf')) {
+      $error = $pdf->getError();
+      // ... handle error here
+    }
+    return new Response($pdf);
   }
 
+  /**
+   * Get image url.
+   *
+   * @param \Drupal\student_pdf\Entity\Student $entity
+   *   Entity Student.
+   * @param string $fieldName
+   *   Field name.
+   *
+   * @return false|string
+   *   Field image url.
+   */
+  public function getImageUri(Student $entity, string $fieldName) {
+
+    $imageField = $entity->get($fieldName)->getValue();
+    if (!empty($imageField[0]['target_id'])) {
+      $file = File::load($imageField[0]['target_id']);
+      // Original URI.
+      return file_create_url($file->getFileUri());
+    }
+    return FALSE;
+  }
 }
